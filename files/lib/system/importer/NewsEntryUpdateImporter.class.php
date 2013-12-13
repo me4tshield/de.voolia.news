9a41fc14d0b9b5d3e4b2fdc340e37b7b80f2f15c
pebeah <itsmepasse@gmail.com>
2013-12-13 23:36:46 +0100

    Added support for news updates import

---
+++ files/lib/system/importer/NewsEntryUpdateImporter.class.php
@@ -0,0 +1,40 @@
+<?php
+namespace news\system\importer;
+use news\data\news\update\NewsUpdate;
+use news\data\news\update\NewsUpdateEditor;
+use wcf\data\user\User;
+use wcf\system\importer\AbstractImporter;
+use wcf\system\importer\ImportHandler;
+use wcf\system\WCF;
+
+/**
+ * Importer for news updates.
+ * 
+ * @author	Pascal Bade
+ * @copyright	2013 voolia.de
+ * @license	Creative Commons CC-BY-ND <http://creativecommons.org/licenses/by-nd/3.0/deed.de>
+ * @package	de.voolia.news
+ */
+class NewsEntryUpdateImporter extends AbstractImporter {
+	/**
+	 * @see	\wcf\system\importer\AbstractImporter::$className
+	 */
+	protected $className = 'news\data\news\update\NewsUpdate';
+
+	/**
+	 * @see	\wcf\system\importer\IImporter::import()
+	 */
+	public function import($oldID, array $data, array $additionalData = array()) {
+		// get user id
+		$data['newsID'] = ImportHandler::getInstance()->getNewID('de.voolia.news.entry', $data['newsID']);
+		$data['userID'] = ImportHandler::getInstance()->getNewID('com.woltlab.wcf.user', $data['userID']);
+
+		// create news update
+		$newsUpdate = NewsUpdateEditor::create($data);
+		$newsUpdateEditor = new NewsUpdateEditor($newsUpdate);
+
+		ImportHandler::getInstance()->saveNewID('de.voolia.news.entry.update', $oldID, $newsUpdate->updateID);
+
+		return $newsUpdate->updateID;
+	}
+}
