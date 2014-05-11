837d27dad3444c475cca3d4bb41e79264796e28a
Pascal Bade <itsmepasse@gmail.com>
2014-05-11 09:43:54 +0200

    Remove unnecessary use statement

--- files/lib/acp/form/NewsPictureAddForm.class.php
+++ files/lib/acp/form/NewsPictureAddForm.class.php
@@ -9,7 +9,6 @@ use wcf\system\exception\IllegalLinkException;
 use wcf\system\exception\UserInputException;
 use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
-use wcf\util\HeaderUtil;
 use wcf\util\StringUtil;
 
 /**
